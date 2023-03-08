0d60eb4721bd1dc2c803786b349df7c861b7939e
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-08 10:57:59 +0100

    Update SinglTodo.tsx

--- src/components/SinglTodo.tsx
+++ src/components/SinglTodo.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import './styles.css';
 import {Todo} from '../model';
 import {AiFillEdit, AiFillDelete} from 'react-icons/ai';
 import {MdDone} from 'react-icons/md';
+import TodoList from './TodoList';
 
 type Props = {
     todo : Todo,
