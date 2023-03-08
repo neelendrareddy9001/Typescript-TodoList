3f5989c4b9800f00e5a49dfc2e07ea391541b1f7
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-08 11:01:06 +0100

    Update SinglTodo.tsx

--- src/components/SinglTodo.tsx
+++ src/components/SinglTodo.tsx
@@ -12,17 +12,32 @@ type Props = {
 }
 
 const SinglTodo : React.FC<Props> = ({todo, todos,setTodos}) => {
+   const SinglTodo : React.FC<Props> = ({todo, todos,setTodos}) => {
+    const handleDone = (id:number) => {
+        setTodos(todos.map((todo) => todo.id === id?{...todo, isDone : !todo.isDone}:todo)
+        )
+    }
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    } 
   return (
     <form className='todos__single'>
+          {
+            todo.isDone ? (
+                <s className='todos__single--text'>{todo.todo}</s>
+            ) : (
+                <span className='todos__single--text'>{todo.todo}</span>
+            )
+          }
         <span className='todos__single--text'>{todo.todo}</span>
         <div>
             <span className='icon'>
                 <AiFillEdit/>
             </span>
-            <span className='icon'>
+            <span className='icon' onClick={() => handleDelete(todo.id)}>
                 <AiFillDelete/>
             </span>
-            <span className='icon'>
+            <span className='icon' onClick={() => handleDone(todo.id}>
                 <MdDone/>
             </span>
         </div>
